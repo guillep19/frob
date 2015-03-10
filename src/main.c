b35e991491822f9acf4b3bf57af68882402a49ec
Guillermo Pacheco <kurco19@gmail.com>
2015-03-09 23:48:38 -0300

    Corrijo implementacion, pruebo operaciones con vmcode.c

--- src/main.c
+++ src/main.c
@@ -2,37 +2,37 @@
 #include <stdint.h>
 
 //VM ops
-#define halt 0x0000
+#define halt 0x00
 //Functions
-#define call 0x0100
-#define ret 0x0200
+#define call 0x01
+#define ret 0x02
 //Tasks
-#define start 0x0300 //running[inmediate] = 1
-#define end 0x0400 //running[inmediate] = 0
+#define start 0x03 //running[inmediate] = 1
+#define end 0x04 //running[inmediate] = 0
 //Jumps
-#define jump 0x0500 //ip = *sp--;
-#define jump_eq 0x0600 //a=*sp--;if (a == *sp--) {ip = *sp--}
-#define jump_neq 0x0700 //a=*sp--;if (a != *sp--) {ip = *sp--}
-#define jump_gt 0x0800 //a=*sp--;if (a > *sp--) {ip = *sp--}
-#define jump_lt 0x0900 //a=*sp--;if (a < *sp--) {ip = *sp--}
+#define jump 0x05 //ip = *sp--;
+#define jump_eq 0x06 //a=*sp--;if (a == *sp--) {ip = *sp--}
+#define jump_neq 0x07 //a=*sp--;if (a != *sp--) {ip = *sp--}
+#define jump_gt 0x08 //a=*sp--;if (a > *sp--) {ip = *sp--}
+#define jump_lt 0x09 //a=*sp--;if (a < *sp--) {ip = *sp--}
 //Binary operators
-#define add 0x0A00 //a=*sp--;*sp += a;
-#define sub 0x0B00 //a=*sp--;*sp -= a;
-#define div 0x0C00 //a=*sp--;*sp = *sp / a;
-#define mul 0x0D00 //a=*sp--;*sp = *sp * a;
-#define and 0x0E00 //a=*sp--;*sp = *sp & a;
-#define or 0x0F00 //a=*sp--;*sp = *sp | a;
+#define add 0x0A //a=*sp--;*sp += a;
+#define sub 0x0B //a=*sp--;*sp -= a;
+#define div 0x0C //a=*sp--;*sp = *sp / a;
+#define mul 0x0D //a=*sp--;*sp = *sp * a;
+#define and 0x0E //a=*sp--;*sp = *sp & a;
+#define or 0x0F //a=*sp--;*sp = *sp | a;
 //Unary operators
-#define neg 0x1000 //*sp = !(*sp)
+#define neg 0x10 //*sp = !(*sp)
 //Stack operations
-#define push 0x1100 //*++sp = inmediate (0..255)
-#define pop 0x1200 //sp--
+#define push 0x11 //*++sp = inmediate (0..255)
+#define pop 0x12 //sp--
 //Memory operations
-#define store 0x1300 //globals[inmediate] = *sp--
-#define load 0x1400 //*++sp = globals[inmediate]
+#define store 0x13 //globals[inmediate] = *sp--
+#define load 0x14 //*++sp = globals[inmediate]
 //Input/Output operations
-#define read 0x1500 //inputs[inmediate] = ip++
-#define write 0x1600 //outputs[inmediate] = *sp--
+#define read 0x15 //inputs[inmediate] = ip++
+#define write 0x16 //outputs[inmediate] = *sp--
 
 typedef uint8_t BYTE;
 typedef uint16_t WORD;
@@ -47,10 +47,14 @@ WORD outputs[16]; //32 bytes
 WORD ip_buffer[32]; //64 bytes
 //Total:1/2 kb (arduino nano tiene 2kb)
 WORD* sp = stack;
-WORD* ip = 0;
+WORD* ip;
 
 BYTE running = 1;
 
+WORD instr = 0;
+BYTE op_code = 0;
+WORD inm = 0;
+WORD aux = 0;
 
 //VM ops
 void f_halt() { running = 0; }
@@ -67,23 +71,30 @@ void f_jump_neq() { WORD a = *sp--; if (a != *sp--) { ip = (WORD*) &code[*sp--];
 void f_jump_gt() { WORD a = *sp--; if (a > *sp--) { ip = (WORD*) &code[*sp--]; } }
 void f_jump_lt() { WORD a = *sp--; if (a < *sp--) { ip = (WORD*) &code[*sp--]; } }
 //Binay
-void f_add() { WORD a = *sp--; *sp = *sp + a; }
-void f_sub() { WORD a = *sp--; *sp = *sp - a; }
-void f_div() { WORD a = *sp--; *sp = *sp / a; }
-void f_mul() { WORD a = *sp--; *sp = *sp * a; }
-void f_and() { WORD a = *sp--; *sp = *sp & a; }
-void f_or() { WORD a = *sp--; *sp = *sp | a; }
+void f_add() {
+  aux = *sp--; *sp = *sp + aux;
+  printf("add: *sp = %d\n", *sp);
+}
+
+void f_sub() {
+  aux = *sp--; *sp = *sp - aux;
+  printf("sub: %d\n", *sp);
+}
+void f_div() { aux = *sp--; *sp = *sp / aux; }
+void f_mul() { aux = *sp--; *sp = *sp * aux; }
+void f_and() { aux = *sp--; *sp = *sp & aux; }
+void f_or() { aux = *sp--; *sp = *sp | aux; }
 //Unary operators
 void f_neg() { *sp = !(*sp); }
 //Stack operations
-void f_push() { *++sp = *ip++; }
+void f_push() { sp++; *sp = inm; }
 void f_pop() { sp--; }
 //Memory operations
-void f_store() { WORD inm = *sp--; globals[inm] = *sp--; }
-void f_load() { WORD inm = *sp--; *++sp = globals[inm]; }
+void f_store() { globals[inm] = *sp--; }
+void f_load() { *++sp = globals[inm]; }
 //Input/Output operations
-void f_read() { WORD inm = *sp--; inputs[inm] = *ip++; /* empty stack and init ip */ }
-void f_write() { WORD inm = *sp--; outputs[inm] = *sp--; }
+void f_read() { ip++; inputs[inm] = ip; /* TODO: empty stack and init ip */ }
+void f_write() { outputs[inm] = *sp--; }
 
 
 void print_instruction(WORD instruction) {
@@ -110,12 +121,27 @@ void print_stack() {
   printf("\n------\n");
 }
 
+
 void run_vm() {
   printf("Running vm....\n");
-  while (running) {
-     
-  }
-  printf("Finished");
+  ip = (WORD*) code;
+    while (running) {
+      instr = *ip;
+      op_code = (0xff00 & instr) >> 8;
+      inm = 0x00ff & instr;
+      if (inm == 0x00ff) { ip++; inm = *ip; }
+      printf("%02x %d\n", op_code, inm);
+      getchar();
+      switch (op_code) {
+        case halt: f_halt(); break;
+        case push: f_push(); break;
+        case add: f_add(); break;
+        case sub: f_sub(); break;
+        default: break;
+      }
+      ip++;
+    }
+  printf("Finished\n");
 }
 
 int main(int argc, char* argv[]) {
--- src/vmcode.c
+++ src/vmcode.c
@@ -1,18 +1,20 @@
 
 extern CODE code = {
-  push | 10,
-  push | 20,
-  add, //30
-  push | 5,
-  sub, //25
-  push | 3,
-  jump, //goto ip+3
-  push | 30,
-  sub, //no lo hace
-  push | 75,
-  add, //25 + 75
-  sub | 4,
-  push | 16,
-  halt
+  push << 8 | 10,
+  push << 8 | 20,
+  add << 8, //30
+  push << 8 | 5,
+  sub << 8, //25
+  push << 8 | 3,
+  jump << 8, //goto ip+3
+  push << 8 | 30,
+  sub << 8, //no lo hace
+  push << 8 | 0xff,
+  75,
+  add << 8, //25 + 75
+  push << 8 | 1,
+  sub << 8,
+  push << 8 | 16,
+  halt << 8
 }; //12 bytes
 
