b45749631732511f974b97d4a97e080087b326b1
Guillermo Pacheco <kurco19@gmail.com>
2015-03-13 01:48:28 -0300

    Completo y corregido un ejemplo que lee un boton y enciende un led. Se probó correctamente con un stub simulando input. Los opcodes estan estabilizandose, luego debo comenzar a implementar un parser o una simplificación del mismo.

--- src/vmcode.c
+++ src/vmcode.c
@@ -25,14 +25,14 @@ extern CODE code = {
   push << 8 | 0,
   store << 8 | 2, //i = 0
   //while:
-  push << 8 | 19,
+  push << 8 | 20,
   push << 8 | 5, //push 5
   load << 8 | 2, //push i
   jump_gt << 8, //i> 5 goto 'end'
-    load << 8 | 0,
+    load << 8 | 2,
     push << 8 | 1,
     add << 8,
-    store << 8 | 1, //i = i + 1
+    store << 8 | 2, //i = i + 1
     read << 8 | 0, // read globals[0] (button)
     store << 8 | 3, //value = read(button)
     push << 8 | 5, //d(jump_neq, else)
@@ -41,13 +41,14 @@ extern CODE code = {
     jump_neq << 8, //goto 'else'
       push << 8 | 1,
       write << 8 | 1, //write(led, 1)
-      push << 8 | 4,
+      push << 8 | 3,
       jump << 8, //goto endif
       //else:
       push << 8 | 0,
       write << 8 | 1, //write(led, 0)
     //'endif'
-    push << 8 | (0x00ff & -22),
+    push << 8 | 0xff,
+    -22,
     jump << 8, //goto while
   //'end'
   halt << 8
