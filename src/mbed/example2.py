351688066608fee9ac00966336bf8cbebf112f65
Guillermo Pacheco <gpacheco@expand.com.uy>
2015-03-16 21:26:38 -0300

    Subo example2 con la implementacion completa en alto nivel de el sguidor de lineasl que cuenta casas. (DEsafio escolar)

--- src/mbed/example2.py
+++ src/mbed/example2.py
@@ -9,33 +9,61 @@ sensor_color_der = 3
 velocidad_motor_izq = 1
 velocidad_motor_der = 2
 
-#Constantes
+#
+# Constantes
+#
+# Al recibir un valor de distancia menor a DISTANCIA_CASA, se considera
+# que se esta frente a una.
 DISTANCIA_CASA = 100
+#
+# Los valores del sensor de color menores e este valor se consideran BLANCO
+UMBRAL_DE_BLANCO = 50 
 
 def hay_casa(distancia):
   return distancia < DISTANCIA_CASA
 
-
-def task_contar_casas()
+def task_contar_casas():
   cuenta = 0
   viendo = False
 
   while cuenta < 2:
-    input_sd = read(sensor_distancia)
-    if not hay_casa(input_sd):
+    distancia = read(sensor_distancia)
+    if not hay_casa(distancia):
       viendo = False
     else:
       viendo = True
       cuenta = cuenta + 1
 
+def es_blanco(color):
+    return color < UMBRAL_DE_BLANCO
 
-button = 0
-led = 1
-i = 0
-while i <= 5:
-  i = i + 1
-  value = read(button)
-  if value == 1:
-    write(led, 1)
-  else:
-    write(led, 0)
+## El camino es negro y el fondo es blanco,  
+## el robot debe tratar de seguir la linea sin
+## salirse.
+def task_seguir_linea():
+  while True:
+    color_izq = read(sensor_color_izq)
+    color_der = read(sensor_color_der)
+    vel_izq = 1
+    vel_der = 1
+    if es_blanco(color_izq):
+        vel_der = 0.5
+    if es_blanco(color_der):
+        vel_izq = 0.5
+    write(velocidad_motor_izq, vel_izq)
+    write(velocidad_motor_der, vel_der)
+
+
+def task_stop_all():
+    vel_izq = 0
+    vel_der = 0
+    write(velocidad_motor_izq, 0)
+    write(velocidad_motor_der, 0)
+
+def main():
+    start(task_contar_casas)
+    start(task_seguir_linea)
+
+    on_finish(task_contar_casas):
+        stop(task_seguir_linea)
+        start(task_stop_all)
