97bdef235ed2a60f982c3a9dcca240141995ed58
Guillermo Pacheco <kurco19@gmail.com>
2015-12-16 00:52:56 -0300

    Add another test

--- src/alfvm/test_code.cpp
+++ src/alfvm/test_code.cpp
@@ -41,6 +41,23 @@ void test_1() {
   assert(*sp == 11);
 }
 
+// Add using a function
+void test_2() {
+  sp = stack;
+  graph = create_graph();
+  CODE test2code = {
+    t_push << 8,
+    6,
+    t_push << 8,
+    5,
+    t_add << 8,
+    t_halt << 8
+  };
+  ip = (WORD*) test2code;
+  run_thread();
+  assert(*sp == 11);
+}
+
 int main() {
   printf("Running tests...\n");
   test_1();
